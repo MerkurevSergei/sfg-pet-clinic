db35e17a0f04eaf212739424eec9dd264bd1cf94
Merkurev Sergei <MerkurevSergei@yandex.ru>
2021-03-14 17:37:18 +0900

    Initial commit

---
+++ src/test/java/ru/merkurev/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkurev.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
